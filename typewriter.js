85cf6068e1d5e1c7d1cbedf777973412e37b8ef8
Daniel Cross <dancross09@gmail.com>
2019-02-24 15:22:54 -0500

    es6 syntax

--- typewriter.js
+++ typewriter.js
@@ -1,51 +1,50 @@
-function TypeWriter(target) {
-    this.el = document.querySelector(target);
-    this.speed = 100; // ms
-    this.q = [];
-    this.isQRunning = false;
+class TypeWriter {
+    constructor(target) {
+        this.el = document.querySelector(target);
+        this.speed = 100; // ms
+        this.q = [];
+        this.isQRunning = false;
+    }
+
+    runQ() {
+        if (!this.isQRunning && this.q.length) {
+            console.log('running q');
+            this.isQRunning = true;
+
+            const intervalId = setInterval(() => {
+                this.el.innerHTML += this.q.shift();
+                console.log(this.q);
+
+                if (!this.q.length) {
+                    console.log('nothing left in q. stopping q. clearing interval.');
+                    clearInterval(intervalId);
+                    this.isQRunning = false;
+                }
+
+            }, this.speed);
+        }
+        else {
+            console.log('either q is running or nothing in it. not running it again.');
+        }
+    }
+
+    write(string) {
+        const characters = string.split('');
+
+        // if q is running
+            // wait until it is not running to
+            // push new elements into the q
+            // so new options can be passed in (such as speed)
+
+        for (let index = 0; index < characters.length; index++) {
+            this.q.push(characters[index]);
+        }
+
+        this.runQ();
+
+        return this;
+    }
 }
-
-TypeWriter.prototype.runQ = function() {
-    if (!this.isQRunning && this.q.length) {
-        console.log('running q');
-        this.isQRunning = true;
-        const self = this;
-
-        const intervalId = setInterval(function(){
-            self.el.innerHTML += self.q.shift();
-            console.log(self.q);
-
-            if (!self.q.length) {
-                console.log('nothing left in q. stopping q. clearing interval.');
-                clearInterval(intervalId);
-                self.isQRunning = false;
-            }
-                
-        }, this.speed);
-    }
-    else {
-        console.log('either q is running or nothing in it. not running it again.');
-    }
-        
-};
-
-TypeWriter.prototype.write = function(string) {
-    const split = string.split('');
-
-    // if q is running
-        // wait until it is not running to
-        // push new elements into the q
-        // so new options can be passed in (such as speed)
-
-    for (let index = 0; index < split.length; index++) {
-        this.q.push(split[index]);
-    }
-
-    this.runQ();
-
-    return this;
-};
-
 
 
 
