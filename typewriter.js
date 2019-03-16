8d56df2082a3dcd9d672f8fcd74a87cd9ce80c73
Daniel Cross <dancross09@gmail.com>
2019-03-16 14:34:29 -0400

    Only check for space before writing next element in charQ, instead of after.

--- typewriter.js
+++ typewriter.js
@@ -29,7 +29,7 @@ const TypeWriter = (function() {
     class TypeWriter {
         constructor(target) {
             this.el = document.querySelector(target);
-            this.speed = 100; // ms
+            this.speed = 250; // ms
             this.charQ = new Queue();
             this.intervalId = undefined;
             this.methodQ = new Queue();
@@ -50,12 +50,14 @@ const TypeWriter = (function() {
             }
 
             this.intervalId = setInterval(() => {
-                target.innerHTML += this.charQ.shift();
-
-                if (this.charQ.peek() === ' ')
+                if (this.charQ.peek() === ' ') {
                     target.innerHTML += this.charQ.shift();
+                }
 
-                if (!this.charQ.getLength()) {
+                if (this.charQ.getLength()) {
+                    target.innerHTML += this.charQ.shift();
+                }
+                else {
                     clearInterval(this.intervalId);
                     this.intervalId = undefined;
                     this.methodQ.isRunning = false;
@@ -84,176 +86,176 @@ const TypeWriter = (function() {
             return this;
         }
 
-        writeWords(text, options) {
-            if (String(text)) {
-                this.methodQ.push(() => {
-                    const words = String(text).split(' ').map((current, index, array) => {
-                        if (index === array.length - 1)
-                            return current;
-                        else
-                            return `${current} `;
-                    });
+        // writeWords(text, options) {
+        //     if (String(text)) {
+        //         this.methodQ.push(() => {
+        //             const words = String(text).split(' ').map((current, index, array) => {
+        //                 if (index === array.length - 1)
+        //                     return current;
+        //                 else
+        //                     return `${current} `;
+        //             });
 
-                    this.charQ.pushSpread(...words);
-                    this.runCharQ(options);
-                });
-            }
+        //             this.charQ.pushSpread(...words);
+        //             this.runCharQ(options);
+        //         });
+        //     }
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        writeAll(text, options) {
-            if (String(text)) {
-                this.methodQ.push(() => {
-                    this.charQ.push(String(text));
-                    this.runCharQ(options);
-                });
-            }
+        // writeAll(text, options) {
+        //     if (String(text)) {
+        //         this.methodQ.push(() => {
+        //             this.charQ.push(String(text));
+        //             this.runCharQ(options);
+        //         });
+        //     }
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        wait(milliseconds) {
-            this.methodQ.push(() => {
-                setTimeout(() => {
-                    this.methodQ.isRunning = false;
-                    this.runNextMethod();
-                }, Math.floor(Math.abs(milliseconds)));
-            });
+        // wait(milliseconds) {
+        //     this.methodQ.push(() => {
+        //         setTimeout(() => {
+        //             this.methodQ.isRunning = false;
+        //             this.runNextMethod();
+        //         }, Math.floor(Math.abs(milliseconds)));
+        //     });
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        newLine() {
-            this.methodQ.push(() => {
-                this.el.innerHTML += '<br/>';
-                this.methodQ.isRunning = false;
-                this.runNextMethod();
-            });
+        // newLine() {
+        //     this.methodQ.push(() => {
+        //         this.el.innerHTML += '<br/>';
+        //         this.methodQ.isRunning = false;
+        //         this.runNextMethod();
+        //     });
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        erase(amount, options = {}) {
-            this.methodQ.push(() => {
-                amount = Math.floor(Math.abs(amount));
+        // erase(amount, options = {}) {
+        //     this.methodQ.push(() => {
+        //         amount = Math.floor(Math.abs(amount));
 
-                if (options.spaces === undefined)
-                    options.spaces = true;
+        //         if (options.spaces === undefined)
+        //             options.spaces = true;
 
-                if (options.speed)
-                    options.speed = Math.floor(Math.abs(options.speed));
+        //         if (options.speed)
+        //             options.speed = Math.floor(Math.abs(options.speed));
 
-                this.intervalId = setInterval(() => {
-                    if (this.el.lastChild && amount) {
-                        if (!this.el.lastChild.textContent.length) {
-                            this.el.lastChild.remove();
-                        }
+        //         this.intervalId = setInterval(() => {
+        //             if (this.el.lastChild && amount) {
+        //                 if (!this.el.lastChild.textContent.length) {
+        //                     this.el.lastChild.remove();
+        //                 }
 
-                        if (this.el.lastChild) {
-                            if (this.el.lastChild.textContent.length) {
-                                this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
-                                amount--;
-                            }
+        //                 if (this.el.lastChild) {
+        //                     if (this.el.lastChild.textContent.length) {
+        //                         this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
+        //                         amount--;
+        //                     }
 
-                            if (options.spaces && amount && this.el.lastChild.textContent.length && this.el.lastChild.textContent[this.el.lastChild.textContent.length - 1] === ' ') {
-                                this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
-                                amount--;
-                            }
-                        }
-                    }
-                    else {
-                        clearInterval(this.intervalId);
-                        this.intervalId = undefined;
-                        this.methodQ.isRunning = false;
-                        this.runNextMethod();
-                    }
-                }, options.speed || this.speed);
-            });
+        //                     if (options.spaces && amount && this.el.lastChild.textContent.length && this.el.lastChild.textContent[this.el.lastChild.textContent.length - 1] === ' ') {
+        //                         this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
+        //                         amount--;
+        //                     }
+        //                 }
+        //             }
+        //             else {
+        //                 clearInterval(this.intervalId);
+        //                 this.intervalId = undefined;
+        //                 this.methodQ.isRunning = false;
+        //                 this.runNextMethod();
+        //             }
+        //         }, options.speed || this.speed);
+        //     });
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        eraseWords(amount, speed) {
-            this.methodQ.push(() => {
-                amount = Math.floor(Math.abs(amount));
-                speed = Math.floor(Math.abs(speed));
+        // eraseWords(amount, speed) {
+        //     this.methodQ.push(() => {
+        //         amount = Math.floor(Math.abs(amount));
+        //         speed = Math.floor(Math.abs(speed));
 
-                this.intervalId = setInterval(() => {
-                    if (this.el.lastChild && amount) {
-                        if (this.el.lastChild.textContent && this.el.lastChild.textContent[this.el.lastChild.textContent.length - 1] === ' ') {
-                            this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
-                        }
+        //         this.intervalId = setInterval(() => {
+        //             if (this.el.lastChild && amount) {
+        //                 if (this.el.lastChild.textContent && this.el.lastChild.textContent[this.el.lastChild.textContent.length - 1] === ' ') {
+        //                     this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
+        //                 }
 
-                        const words = this.el.lastChild.textContent.split(' ');
-                        const lastWord = words[words.length - 1];
+        //                 const words = this.el.lastChild.textContent.split(' ');
+        //                 const lastWord = words[words.length - 1];
 
-                        this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -lastWord.length);
+        //                 this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -lastWord.length);
 
-                        if (this.el.lastChild.textContent && this.el.lastChild.textContent[this.el.lastChild.textContent.length - 1] === ' ') {
-                            this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
-                        }
+        //                 if (this.el.lastChild.textContent && this.el.lastChild.textContent[this.el.lastChild.textContent.length - 1] === ' ') {
+        //                     this.el.lastChild.textContent = this.el.lastChild.textContent.slice(0, -1);
+        //                 }
 
-                        if (!this.el.lastChild.textContent) {
-                            this.el.lastChild.remove();
-                        }
+        //                 if (!this.el.lastChild.textContent) {
+        //                     this.el.lastChild.remove();
+        //                 }
 
-                        amount--;
-                    }
-                    else {
-                        clearInterval(this.intervalId);
-                        this.intervalId = undefined;
-                        this.methodQ.isRunning = false;
-                        this.runNextMethod();
-                    }
-                }, speed || this.speed);
-            });
+        //                 amount--;
+        //             }
+        //             else {
+        //                 clearInterval(this.intervalId);
+        //                 this.intervalId = undefined;
+        //                 this.methodQ.isRunning = false;
+        //                 this.runNextMethod();
+        //             }
+        //         }, speed || this.speed);
+        //     });
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        eraseAll() {
-            this.methodQ.push(() => {
-                this.el.innerHTML = '';
-                this.methodQ.isRunning = false;
-                this.runNextMethod();
-            });
+        // eraseAll() {
+        //     this.methodQ.push(() => {
+        //         this.el.innerHTML = '';
+        //         this.methodQ.isRunning = false;
+        //         this.runNextMethod();
+        //     });
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        callBack(cb, args) {
-            this.methodQ.push(() => {
-                if (args)
-                    cb(...args);
-                else
-                    cb();
+        // callBack(cb, args) {
+        //     this.methodQ.push(() => {
+        //         if (args)
+        //             cb(...args);
+        //         else
+        //             cb();
 
-                this.methodQ.isRunning = false;
-                this.runNextMethod();
-            });
+        //         this.methodQ.isRunning = false;
+        //         this.runNextMethod();
+        //     });
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
 
-        setSpeed(speed) {
-            this.methodQ.push(() => {
-                this.speed = Math.floor(Math.abs(speed));
-                this.methodQ.isRunning = false;
-                this.runNextMethod();
-            });
+        // setSpeed(speed) {
+        //     this.methodQ.push(() => {
+        //         this.speed = Math.floor(Math.abs(speed));
+        //         this.methodQ.isRunning = false;
+        //         this.runNextMethod();
+        //     });
 
-            this.runNextMethod();
-            return this;
-        }
+        //     this.runNextMethod();
+        //     return this;
+        // }
     }
 
     return TypeWriter;
