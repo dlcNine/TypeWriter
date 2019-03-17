58be0b3f0d857de442172be9bcf357748902cb03
Daniel Cross <dancross09@gmail.com>
2019-03-17 13:18:35 -0400

    Example for erasing

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,17 +4,65 @@ summary.wait(500).write('A small JS library to expressively render tetx.').wait(
 const writeExample = new TypeWriter('#writeExample');
 
 document.querySelector('#triggerWriteExample').addEventListener('click', function(){
-    if (!writeExample.methodQ.getLength()) {
-        writeExample.eraseAll().write('the quick brown fox jumps over the lazy dog.');
-        writeExample.eraseAll().write('the quick brown fox jumps over the lazy dog.', {speed: 150, class: 'text-underline'});
+    if (!writeExample.methodQ.isRunning) {
+        writeExample.eraseAll().write('The quick brown fox jumps over the lazy dog.');
+        writeExample.eraseAll().write('The quick brown fox jumps over the lazy dog.', {speed: 150, class: 'text-underline'});
     }
 });
 
 const writeWordsExample = new TypeWriter('#writeWordsExample');
 
 document.querySelector('#triggerWriteWordsExample').addEventListener('click', function(){
-    if (!writeWordsExample.methodQ.getLength()) {
-        writeWordsExample.eraseAll().writeWords('the quick brown fox jumps over the lazy dog.');
-        writeWordsExample.eraseAll().writeWords('the quick brown fox jumps over the lazy dog.', {speed: 300, class: 'text-underline'});
+    if (!writeWordsExample.methodQ.isRunning) {
+        writeWordsExample.eraseAll().writeWords('The quick brown fox jumps over the lazy dog.');
+        writeWordsExample.eraseAll().writeWords('The quick brown fox jumps over the lazy dog.', {speed: 300, class: 'text-underline'});
     }
 });
+
+const writeAllExample = new TypeWriter('#writeAllExample');
+
+document.querySelector('#triggerWriteAllExample').addEventListener('click', function(){
+    if (!writeAllExample.methodQ.isRunning) {
+        writeAllExample.eraseAll().writeAll('The quick brown fox jumps over the lazy dog.').wait(1000);
+        writeAllExample.eraseAll().writeAll('The quick brown fox jumps over the lazy dog.', {speed: 300, class: 'text-underline'});
+    }
+});
+
+const eraseExample = new TypeWriter('#eraseExample');
+
+document.querySelector('#triggerEraseExample').addEventListener('click', function(){
+    if (!eraseExample.methodQ.isRunning) {
+        eraseExample.eraseAll().write('The quick brown').write(' fox jumps over', {class: 'text-underline'}).write(' the lazy dog.').erase(35).wait(1000);
+        eraseExample.eraseAll().wait(500).write('The quick brown').write(' fox jumps over', {class: 'text-underline'}).write(' the lazy dog.').erase(35, 150);
+    }
+});
+
+const eraseWordsExample = new TypeWriter('#eraseWordsExample');
+
+document.querySelector('#triggerEraseWordsExample').addEventListener('click', function(){
+    if (!eraseWordsExample.methodQ.isRunning) {
+        eraseWordsExample.eraseAll().write('The quick brown').write(' fox jumps over', {class: 'text-underline'}).write(' the lazy dog.').eraseWords(7).wait(500);
+        eraseWordsExample.eraseAll().wait(500).write('The quick brown').write(' fox jumps over', {class: 'text-underline'}).write(' the lazy dog.').eraseWords(7, 500);
+    }
+});
+
+const eraseAllExample = new TypeWriter('#eraseAllExample');
+
+document.querySelector('#triggerEraseAllExample').addEventListener('click', function(){
+    if (!eraseAllExample.methodQ.isRunning) {
+        eraseAllExample.write(' The quick brown fox jumps over the lazy dog.').wait(500).eraseAll();
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
